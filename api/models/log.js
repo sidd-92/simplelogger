40f4d364254cc5a47cd2ef01db5e2b65d66c121c
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2019-08-31 22:37:26 +0530

    Completed Basic Routes For the Api

--- api/models/log.js
+++ api/models/log.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
+const moment = require("moment");
 const logSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  date: { type: Date, required: true, default: Date.now },
-  mealType: { type: String, required: true }
+  date: {
+    type: Date,
+    required: true,
+    default: new Date()
+  },
+  mealType: { type: String, required: true, default: "None" },
+  isBeverage: { type: Boolean, default: false },
+  totalResident: { type: Number, required: true, default: 0 },
+  totalGuest: { type: Number, required: true, default: 0 },
+  totalHD: { type: Number, required: true, default: 0 }
 });
 
 module.exports = mongoose.model("Log", logSchema);
--- api/routes/log.js
+++ api/routes/log.js
@@ -4,17 +4,72 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const Log = require("../models/log");
+const moment = require("moment");
+
+/*
+Log Class
+_id: mongoose.Schema.Types.ObjectId,
+date: { type: Date, required: true, default: Date.now },
+mealType: { type: String, required: true, default: "None" },
+isBeverage: { type: Boolean, required: true, default: true },
+totalResident: { type: Number, required: true, default: 0 },
+totalGuest: { type: Number, required: true, default: 0 },
+totalHD: { type: Number, required: true, default: 0 } 
+*/
+/*
+POST Object Notation
+{
+  date: ,
+  mealType: ,
+  isBeverage: ,
+  totalResident: ,
+  totalGuest: ,
+  totalHD: ,
+}
+
+*/
 
 //GET LOGS
 router.get("/", (req, res, next) => {
   Log.find()
-    .select("mealType date _id")
+    .select("-_v")
     .exec()
     .then(result => {
       const response = {
         count: result.length,
         logs: result.map(doc => {
           return {
+            isBeverage: doc.isBeverage,
+            totalResident: doc.totalResident,
+            totalGuest: doc.totalGuest,
+            totalHD: doc.totalHD,
+            mealType: doc.mealType,
+            dateLogged: doc.date,
+            _id: doc._id
+          };
+        })
+      };
+      res.status(200).json(response);
+    })
+    .catch(err => console.log(err));
+});
+//Get Data between Dates
+router.get("/filterbydate", (req, res, next) => {
+  let { startDate, endDate } = req.query;
+  Log.find({
+    date: { $lte: endDate, $gte: startDate }
+  })
+    .select("-_v")
+    .exec()
+    .then(result => {
+      const response = {
+        count: result.length,
+        logs: result.map(doc => {
+          return {
+            isBeverage: doc.isBeverage,
+            totalResident: doc.totalResident,
+            totalGuest: doc.totalGuest,
+            totalHD: doc.totalHD,
             mealType: doc.mealType,
             dateLogged: doc.date,
             _id: doc._id
@@ -37,7 +92,11 @@ router.post("/", (req, res, next) => {
   const newLog = new Log({
     _id: new mongoose.Types.ObjectId(),
     date: req.body.date,
-    mealType: req.body.mealType
+    mealType: req.body.mealType,
+    isBeverage: req.body.isBeverage,
+    totalResident: req.body.totalResident,
+    totalGuest: req.body.totalGuest,
+    totalHD: req.body.totalHD
   });
   newLog
     .save()
@@ -49,7 +108,11 @@ router.post("/", (req, res, next) => {
           createdLog: {
             _id: new mongoose.Types.ObjectId(),
             date: req.body.date,
-            mealType: req.body.mealType
+            mealType: req.body.mealType,
+            isBeverage: req.body.isBeverage,
+            totalResident: req.body.totalResident,
+            totalGuest: req.body.totalGuest,
+            totalHD: req.body.totalHD
           }
         });
       } else {
@@ -61,6 +124,23 @@ router.post("/", (req, res, next) => {
     .catch(err => {
       console.log(err);
       res.status(500).json({
+        error: err
+      });
+    });
+});
+router.delete("/:logID", (req, res, next) => {
+  const id = req.params.logID;
+  Log.deleteOne({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: "Log Deleted"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: "Given ID Not Available In DB",
         error: err
       });
     });
@@ -83,8 +163,5 @@ router.patch("/:logID", (req, res, next) => {
   res.status(200).json(response);
 });
 
-router.delete("/:logID", (req, res, next) => {
-  const id = req.params.logID;
-  res.status(200).json(response);
-}); */
+ */
 module.exports = router;
