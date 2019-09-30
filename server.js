62954caafacb61265d98c2e94910635e1bb0fbd9
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2019-09-30 20:41:56 +0530

    production build setup

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const http = require("http");
-const app = require("./app");
+const express = require("express");
+const app = express();
 const PORT = process.env.PORT || 5000;
-const server = http.createServer(app);
-server.listen(PORT, console.log(`Server Started at ${PORT}`));
+const path = require("path");
+const connectDB = require("./config/db");
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+app.use("/api/logs", require("./routes/api/log"));
+
+//Serve Static Assets in Production
+if (process.env.NODE_ENV === "production") {
+  //Set a Static Folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log("http://localhost:5000");
+});
